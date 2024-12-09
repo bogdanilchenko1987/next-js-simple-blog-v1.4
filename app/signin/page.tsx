9a8bd69007ba7a2825b17043233b4eac77b85657
bogdanilchenko1987 <bogdan_ilchenko@ukr.net>
2024-12-09 05:16:42 +0200

    init2

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,14 +1,14 @@
 import { GoogleButton } from "@/components/GoogleButton";
 import { SignInForm } from "@/components/SignInForm";
-// import { Suspense } from "react";
+import { Suspense } from "react";
 
 export default async function SignIn() {
   return (
     <div className="stack">
       <h1>SignIn</h1>
-      {/* <Suspense> */}
-      <GoogleButton />
-      {/* </Suspense> */}
+      <Suspense>
+        <GoogleButton />
+      </Suspense>
       <div>or</div>
       <SignInForm />
     </div>
